ab534a4066d926561525f6ab68da1cf37534744a
lianpeng.liu <lianpeng.liu@drcuiyutao.com>
2018-09-16 15:57:38 +0800

    v1.0.11

---
+++ src/alert/alert.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="vux-alert" v-if="showValue">
+    <div>
+      <div class="weui-dialog__hd">
+        <strong class="weui-dialog__title">{{ title }}</strong>
+      </div>
+      <div class="weui-dialog__bd">
+        <slot>
+          <div v-html="content"></div>
+        </slot>
+      </div>
+      <div class="weui-dialog__ft">
+        <a href="javascript:;"
+          class="weui-dialog__btn weui-dialog__btn_primary"
+          @click="_onHide">{{ buttonText || "好的" }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "alert",
+  components: {},
+  created() {
+    if (typeof this.value !== "undefined") {
+      this.showValue = this.value;
+    }
+  },
+  props: {
+    value: Boolean,
+    title: String,
+    content: String,
+    buttonText: String,
+    hideOnBlur: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      showValue: false
+    };
+  },
+  methods: {
+    _onHide() {
+      this.showValue = false;
+    }
+  },
+  watch: {}
+};
+</script>
+
+<style lang="sass">
+</style>
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Alert from './alert/index.js';
+import Alert2 from './alert/index.js';
+
+
+const components = [
+  Alert, Alert2
+]
+
+const install = function (Vue, opts = {}) {
+  components.map(component => {
+    Vue.component(component.name, component);
+  })
+}
+
+/* 支持使用标签的方式引入 */
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+
+
+export default {
+  install,
+  Alert,
+  Alert2
+};
